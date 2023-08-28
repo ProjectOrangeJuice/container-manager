53ab97e6117290678af07d17495c9abd8cfcd2e5
ProjectOrange <theJuggernaut@harriso.co.uk>
2023-08-28 15:24:04 +0100

    add config for client

--- client/main.go
+++ client/main.go
@@ -9,11 +9,33 @@ import (
 	"io"
 	"log"
 	"net"
+	"os"
 	"strings"
 	"time"
 )
 
 func main() {
+
+	// read file to check if this is the first run
+
+	// if this is the first run, create a new key pair
+	config, exists, err := readConfig()
+	if err != nil {
+		if exists {
+			log.Printf("Error reading config file, %s. As the file exists, we won't create it", err)
+			return
+		}
+		log.Printf("Config file was not there, running setup [%s]", err)
+		err = firstRun()
+		if err != nil {
+			log.Printf("First run failed, %s", err)
+			return
+		}
+		return
+	}
+
+	log.Printf("Config [%+v]", config)
+
 	// Define the server address and port.
 	addr := "localhost:8080"
 
@@ -124,4 +146,50 @@ func createEvent[R any](request string, result R) ([]byte, error) {
 		return nil, fmt.Errorf("could not marshal event, %s", err)
 	}
 	return eventOut, nil
+}
+
+type Config struct {
+	Name              string
+	KeyLocation       string
+	ServerAddress     string
+	ServerFingerprint string
+}
+
+func firstRun() error {
+	config := Config{
+		Name:              "Test client",
+		KeyLocation:       "./keys",
+		ServerAddress:     "localhost:8080",
+		ServerFingerprint: "test",
+	}
+
+	// write json to file
+	file, err := os.Create("config.json")
+	if err != nil {
+		return fmt.Errorf("could not create config file, %s", err)
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "\t")
+	err = encoder.Encode(config)
+	if err != nil {
+		return fmt.Errorf("could not encode config file, %s", err)
+	}
+	return nil
+}
+
+func readConfig() (Config, bool, error) {
+	file, err := os.Open("config.json")
+	if err != nil {
+		return Config{}, false, fmt.Errorf("could not open config file, %s", err)
+	}
+	defer file.Close()
+
+	config := Config{}
+	err = json.NewDecoder(file).Decode(&config)
+	if err != nil {
+		return Config{}, true, fmt.Errorf("could not decode config file, %s", err)
+	}
+	return config, true, nil
 }
