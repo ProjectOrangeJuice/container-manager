6978628a05f22727eee59b3ce576344379fc4861
ProjectOrange <theJuggernaut@harriso.co.uk>
2023-10-17 22:44:50 +0100

    create API version

--- web/web.go
+++ web/web.go
@@ -20,6 +20,8 @@ func StartWebServer(clients connection.Clients) {
 	r := gin.Default()
 	r.LoadHTMLGlob("web/templates/*")
 	r.GET("/", w.handleList)
+	r.OPTIONS("/api/list", w.handleListAPI)
+	r.GET("/api/list", w.handleListAPI)
 
 	r.Run(":8081")
 }
@@ -60,4 +62,33 @@ func (w webServer) handleList(c *gin.Context) {
 	log.Printf("Web result: %+v", webResult)
 
 	c.HTML(http.StatusOK, "index", webResult)
+}
+
+func (w webServer) handleListAPI(c *gin.Context) {
+	// Set CORS headers
+	c.Header("Access-Control-Allow-Origin", "*")
+	c.Header("Access-Control-Allow-Headers", "Content-Type")
+
+	clients := w.clients.GetAllClients()
+	webResult := []clientResult{}
+	for _, cc := range clients {
+		client := clientResult{
+			Name:    cc.Name,
+			CPU:     cc.System.CPUUseage,
+			Memory:  (float64(cc.System.TotalMemory-cc.System.FreeMemory) / float64(cc.System.TotalMemory)) * 100,
+			Storage: []storageResult{},
+		}
+		for _, storage := range cc.Storage {
+			client.Storage = append(client.Storage, storageResult{
+				Name:      storage.Name,
+				Mount:     storage.Mount,
+				SpaceUsed: (storage.UsedSpace / storage.TotalSpace) * 100,
+			})
+		}
+		webResult = append(webResult, client)
+	}
+
+	log.Printf("Web result: %+v", webResult)
+
+	c.JSON(http.StatusOK, webResult)
 }
