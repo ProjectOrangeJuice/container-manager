643e2b34f3a89b1465669f501d24f0be2f993626
ProjectOrange <theJuggernaut@harriso.co.uk>
2023-10-28 22:16:46 +0100

    remove web page

--- web/web.go
+++ web/web.go
@@ -18,8 +18,6 @@ func StartWebServer(clients connection.Clients) {
 	}
 
 	r := gin.Default()
-	r.LoadHTMLGlob("web/templates/*")
-	r.GET("/", w.handleList)
 	r.OPTIONS("/api/list", w.handleListAPI)
 	r.GET("/api/list", w.handleListAPI)
 
@@ -37,31 +35,6 @@ type storageResult struct {
 	Name      string
 	Mount     string
 	SpaceUsed float64
-}
-
-func (w webServer) handleList(c *gin.Context) {
-	clients := w.clients.GetAllClients()
-	webResult := []clientResult{}
-	for _, cc := range clients {
-		client := clientResult{
-			Name:    cc.Name,
-			CPU:     cc.System.CPUUseage,
-			Memory:  (float64(cc.System.TotalMemory-cc.System.FreeMemory) / float64(cc.System.TotalMemory)) * 100,
-			Storage: []storageResult{},
-		}
-		for _, storage := range cc.Storage {
-			client.Storage = append(client.Storage, storageResult{
-				Name:      storage.Name,
-				Mount:     storage.Mount,
-				SpaceUsed: (storage.UsedSpace / storage.TotalSpace) * 100,
-			})
-		}
-		webResult = append(webResult, client)
-	}
-
-	log.Printf("Web result: %+v", webResult)
-
-	c.HTML(http.StatusOK, "index", webResult)
 }
 
 func (w webServer) handleListAPI(c *gin.Context) {
